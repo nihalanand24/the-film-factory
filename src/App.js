79ff1d0eeac31f1bbbde1023ad0a4031e1a6a0cb
nihalanand <nihalanand@gmail.com>
2021-03-11 17:48:49 -0500

    tested API and console.logged similar movies per page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
 
-function App() {
+function App () {
+
+
+  // const baseUrl = 'https://api.themoviedb.org/3';
+  // const apiKey = `0f71218e40b140c550833011fa9c4afb`;
+  // const searchMovie = '/search/movie';
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: 'https://api.themoviedb.org/3/search/movie',
+      dataResponse: 'JSON',
+      params: {
+        api_key: '0f71218e40b140c550833011fa9c4afb',
+        query: 'Interstellar'
+      }
+    }).then(res => getMovies(res.data.results[0].id))
+  }, [])
+
+  const getMovies = (id) => {
+    axios({
+      method: 'GET',
+      url: `https://api.themoviedb.org/3/movie/${id}/similar`,
+      dataResponse: 'JSON',
+      params: {
+        api_key: '0f71218e40b140c550833011fa9c4afb',
+        page: 1
+      }
+    }).then(res => res.data.results).then(arr => arr.filter(movie => movie.original_language !== 'en')).then(foreignFilmArr => foreignFilmArr.forEach(film => console.log(film.title, film.id, film.original_language)))
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>TESTING APP</p>
+      <button type='submit'>CLICK ME</button>
     </div>
   );
 }
