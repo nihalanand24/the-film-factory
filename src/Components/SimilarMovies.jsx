a3ecd7e3f6b8c0ea89182bacd1b8abe513d5b19c
nihalanand <nihalanand@gmail.com>
2021-03-14 19:19:19 -0400

    fixed similarMovies and added getSimilarMovies component

--- src/Components/SimilarMovies.jsx
+++ src/Components/SimilarMovies.jsx
@@ -1,7 +1,8 @@
 // SimilarMovies
-import axios from 'axios';
+// import axios from 'axios';
 import { useState, useEffect } from 'react';
-import languageArray from './languageArray';
+import getSimilarMovies from './getSimilarMovies';
+// import languageArray from './languageArray';
 import MovieCard from './MovieCard';
 // import ISO6391 from 'iso-639-1';
 
@@ -9,60 +10,15 @@ const SimilarMovies = ({ id }) => {
   const [movieSuggestions, setMovieSuggestions] = useState([]);
 
   useEffect(() => {
-    const foreignMovieArr = [];
-
     if (id) {
-      for (let i = 1; i < 51; i++) {
-            axios({
-              method: 'GET',
-              url: `https://api.themoviedb.org/3/movie/${id}/similar`,
-              dataResponse: 'JSON',
-              params: {
-                api_key: '0f71218e40b140c550833011fa9c4afb',
-                page: i,
-              },
-            })
-              .then((res) => {
-                const unfilteredMovies = res.data.results;
-                const filteredMovies = unfilteredMovies.filter(
-                  (movie) => movie.original_language !== 'en'
-                );
-                // return filteredMovies;
-                filteredMovies.forEach((movie) => {
-                  Promise.resolve(languageArray).then(res => {
-                    res.forEach(lang => {
-                      if (lang.iso_639_1 === movie.original_language) {
-                        if (foreignMovieArr.length < 5) {
-                            foreignMovieArr.push({...movie, language: lang.english_name});
-                        } 
-                      }
-                    })
-                  });
-
-                  });
-                  //   return foreignMovieArr;
-                  if (foreignMovieArr.length === 5 || i === 50) {
-                      setMovieSuggestions(foreignMovieArr);
-                      // console.log(foreignMovieArr);
-                    }
-
-
-                })
-
-      }
+      setMovieSuggestions([]);
+      getSimilarMovies(id, setMovieSuggestions);
     }
-    // Filter results for lang != en
-    // Find 4 movies
-    // then push results to tempArray
-
-    // setMovieSuggestArr(res.data.results);
   }, [id]);
 
-
   const sayMyName = (movie) => {
-    console.log(movie.title, movie.original_language, movie.poster_path)
-  }
-
+    console.log(movie.title, movie.original_language, movie.poster_path);
+  };
 
   return (
     <>
@@ -70,11 +26,7 @@ const SimilarMovies = ({ id }) => {
 
       {movieSuggestions.length &&
         movieSuggestions.map((movie) => {
-
-
           return (
-            // <h3>{movie.title}</h3>
-
             <MovieCard
               key={movie.id}
               movie={movie}
